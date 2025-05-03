d0170bd8af065ad463500d0f70fd8b08b8ba2826
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2025-05-03 11:10:52 -0300

    chore(dependencies): agrega nodemailer a package.json y package-lock.json; elimina la versión de Docker en docker-compose.yml y docker-compose.prod.yml.

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ import eventoRoutes from "./routes/evento.routes.js";
 import tipoEntradaRoutes from "./routes/tipoEntrada.routes.js";
 import entradasRoutes from "./routes/entradas.routes.js";
 import roleAsigneeRoutes from "./routes/roleAsignee.routes.js";
-import { generatePaymentLink } from "./controllers/payment.controller.js";
-import authMiddleware from "./middlewares/auth.middleware.js";
 import profileRoutes from "./routes/profile.routes.js";
 const app = express();
 const port = process.env.PORT || 3000;
