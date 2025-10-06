5e1aa8bb18af503f947d683bb117c96579a85742
Fabio Mazzarella <fabiomazzarella97@hotmail.com>
2025-10-06 19:55:36 -0300

    fix import rutas

--- src/routes/evento.routes.js
+++ src/routes/evento.routes.js
@@ -6,17 +6,21 @@ import {
   updateEvento,
   getEventosByProductora,
   deleteEvento,
-  getMyEventos
+  getMyEventos,
 } from "../controllers/evento.controller.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.get("/:id", getEventoById);
+// Rutas específicas primero (antes de las rutas con parámetros dinámicos)
 router.get("/", getAllEventos);
-router.get("/my-eventos", authenticateToken, getMyEventos); 
+router.get("/my-eventos", authenticateToken, getMyEventos);
 router.get("/productora/:id", getEventosByProductora);
 router.post("/", createEvento);
-router.put("/:id", updateEvento);       
+
+// Rutas con parámetros dinámicos al final
+router.get("/:id", getEventoById);
+router.put("/:id", updateEvento);
 router.delete("/:id", deleteEvento);
+
 export default router;
